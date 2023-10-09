a8e4d1470fbb030b8c5528004a5af3491716d783
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-10-09 14:04:02 +0900

    add コメント

--- main.go
+++ main.go
@@ -34,10 +34,17 @@ func generator(ctx context.Context, num int) <-chan int {
 
 func main() {
 
-	// チェネルを作成する
-	// doneChannel := make(chan string)
+	// contextの初期化
+	// context.Background()関数の返り値からは、「キャンセルされない」「deadlineも持たない」「共有する値も何も持たない」状態のcontextが得られます。
+	// いわば「context初期化のための関数」です。
+	// https://zenn.dev/hsaki/books/golang-context/viewer/done#context%E3%81%AE%E5%88%9D%E6%9C%9F%E5%8C%96
+	initialCtx := context.Background()
 
-	ctx, cancel := context.WithCancel(context.Background())
+	// contextにキャンセル機能を追加
+	// context.Background()から得たまっさらなcontextをcontext.WithCancel()関数に渡すことで、
+	// 「Doneメソッドからキャンセル有無が判断できるcontext」と「第一返り値のコンテキストをキャンセルするための関数」を得ることができます。
+	// https://zenn.dev/hsaki/books/golang-context/viewer/done#context%E3%81%AB%E3%82%AD%E3%83%A3%E3%83%B3%E3%82%BB%E3%83%AB%E6%A9%9F%E8%83%BD%E3%82%92%E8%BF%BD%E5%8A%A0
+	ctx, cancel := context.WithCancel(initialCtx)
 
 	gen := generator(ctx, 1)
 
